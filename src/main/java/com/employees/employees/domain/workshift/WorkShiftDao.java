0f5d5eb54d0acc55894bfb189e0c74a610cd79c0
edenmaria <edenmaria@gmail.com>
2019-05-16 17:20:11 -0400

    Fixed sql cast date

--- src/main/java/com/employees/employees/domain/workshift/WorkShiftDao.java
+++ src/main/java/com/employees/employees/domain/workshift/WorkShiftDao.java
@@ -11,7 +11,7 @@ public interface WorkShiftDao extends CrudRepository<WorkShift, List> {
     List<WorkShift> findById(Long id);
 
     @Query(value = "SELECT * FROM workshifts "+
-				   "WHERE dias LIKE CONCAT('%',:dias,'%') AND hora_inicio LIKE CONCAT('%',:hora_inicio,'%') ", nativeQuery = true)
+				   "WHERE dias LIKE CONCAT('%',:dias,'%') AND hora_inicio LIKE CONCAT('%',cast(:hora_inicio as time),'%') ", nativeQuery = true)
 	public ArrayList<WorkShift> findWorkShift(@Param("dias") String dias, @Param("hora_inicio") String hora_inicio);
     // @Query(value = "SELECT * FROM workshifts "+
 	// 			   "WHERE dias=:dias AND hora_inicio=:cast(:hora_inicio as DATE) AND hora_fin=:cast(:hora_fin as DATE)", nativeQuery = true)
