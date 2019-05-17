4e942db4ae1bbb3fa3f42197a1f8f1950e156f78
edenmaria <edenmaria@gmail.com>
2019-05-17 01:03:54 -0400

    Fixed endpoint workshifts-filters

--- src/main/java/com/employees/employees/domain/workshift/WorkShiftDao.java
+++ src/main/java/com/employees/employees/domain/workshift/WorkShiftDao.java
@@ -12,8 +12,8 @@ public interface WorkShiftDao extends CrudRepository<WorkShift, List> {
    
     @Query(value = "SELECT * FROM workshifts "+
 				   "WHERE dias LIKE CONCAT('%',:dias,'%') "+ 
-				   "AND (hora_inicio LIKE CONCAT('%',cast(:hora_inicio as time),'%') OR length(hora_inicio) != 0) "+ 
-				   "AND (hora_fin LIKE CONCAT('%',cast(:hora_fin as time),'%') OR length(hora_fin) != 0)", nativeQuery = true)
+				   "AND (hora_inicio LIKE CONCAT('%',:hora_inicio,'%')) "+ 
+				   "AND (hora_fin LIKE CONCAT('%',:hora_fin,'%'))", nativeQuery = true)
 	public ArrayList<WorkShift> findWorkShift(@Param("dias") String dias,@Param("hora_inicio") String hora_inicio,@Param("hora_fin") String hora_fin);
     
 } 
